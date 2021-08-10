21dbc48747216b4428579b1e246330acb72362b6
renjval12 <reneejoyvalentine3@gmail.com>
2021-08-10 17:56:12 -0400

    prompt sync json files

--- Week 3/functionHw_paramsCounter.js
+++ Week 3/functionHw_paramsCounter.js
@@ -1,6 +1,6 @@
 //task # 5 - Write a function that returns the number of arguments passed to the function when called.
 
-let prompt = (require('prompt-sync'))()
+let prompt = require('prompt-sync')();
 let string = prompt('Please type in the name of your favorite song: ').split(' ')
 
 //solution #1 = use of arguments object
@@ -9,7 +9,7 @@ function paramCounter() {
 }
 
 //solution #2 = use of .length method
-//paramCounter = () => string.length
+// paramCounter = (...string) => string.length
 
 //not entirely sure which solution is best practice. Included both.
 
