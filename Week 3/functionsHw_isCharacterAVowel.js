e39c9f3b41450193df3f4a3e43d29c2dd6b30e6a
renjval12 <reneejoyvalentine3@gmail.com>
2021-08-10 18:51:13 -0400

    is Character a vowel updated

--- Week 3/functionsHw_isCharacterAVowel.js
+++ Week 3/functionsHw_isCharacterAVowel.js
@@ -1,7 +1,26 @@
 //task # 3 - check if a character is a vowel
 
-let char = 'z'
+let char = 'a'
 
-isCharacterAVowel = () => char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ? `'${char}' is a vowel.` : `'${char}' is a consonant.`
+isCharacterAVowel = () => {
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+        return `'${char}' is a vowel.`
+    }
+    else {
+        return `'${char}' is a consonant.`
+    }
+}
 
-console.log(isCharacterAVowel())
+
+if (char.length === 1 && typeof char === 'string') {
+    console.log(isCharacterAVowel())
+}
+else if (char.length > 1) {
+    console.log('you entered too many characters')
+}
+else if (typeof char == 'number') {
+    console.log('you entered a number')
+}
+else {
+    console.log('invalid entry')
+}
