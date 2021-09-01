f5ec72b7dfd3b84c0ff9b066c208465d56c2313c
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-01 03:55:03 -0400

    add image

--- Week6/static/src/bottomContent.js
+++ Week6/static/src/bottomContent.js
@@ -3,7 +3,7 @@ export default function BottomComp() {
         <div id="img-container">
             <p>Website Visitors</p>
             <p>821</p>
-            <img src="" alt="elon musk meme"/>
+            <img src="https://cdn.britannica.com/84/73184-004-E5A450B5/Sunflower-field-Fargo-North-Dakota.jpg" alt="sunflower meadow"/>
         </div>
     )
 }
