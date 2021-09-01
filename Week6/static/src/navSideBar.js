056bec1a5314a132c7bd8fb728d2ebc2209ebc87
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-01 10:49:08 -0400

    fixed margins

--- Week6/static/src/navSideBar.js
+++ Week6/static/src/navSideBar.js
@@ -1,4 +1,3 @@
-
 export default function NavSideBar() {
     return (
         <div id="side-bar">
--- Week6/static/src/topContentComp.js
+++ Week6/static/src/topContentComp.js
@@ -1,18 +1,18 @@
-import React from 'react'
+//a react function component, that calls the TopComps react component 3 times inside of it.
 
 export default function TopContent() {
     return (
         <div id="top-content">
-            <TopComps title="Reviews" num1={1281} />
-            <TopComps title="Average Rating" num1={4.6} />
-            <TopComps title="Sentiment Analysis" num1={Math.floor(Math.random() * 900)} num2={Math.floor(Math.random() * 900)} num3={Math.floor(Math.random() * 900)} />
+            <TopComps idnum={1}title="Reviews" num1={1281} />
+            <TopComps idnum={2}title="Average Rating" num1={4.6} />
+            <TopComps idnum={3}title="Sentiment Analysis" num1={Math.floor(Math.random() * 900)} num2={Math.floor(Math.random() * 900)} num3={Math.floor(Math.random() * 900)} />
         </div>)
-
 }
 
+//react function component that returns a div
 function TopComps(props) {
     return (
-        <div>
+        <div id={'top-comp-'+ props.idnum}>
             <h3>{props.title}</h3>
             <p>{props.num1}</p>
             <p>{props.num2}</p>
