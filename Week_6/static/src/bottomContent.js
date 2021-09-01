eac8667fbb034661a34f57c33edbf49ceb208247
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-01 03:49:50 -0400

    changed image

--- Week_6/static/src/bottomContent.js
+++ Week_6/static/src/bottomContent.js
@@ -3,7 +3,7 @@ export default function BottomComp() {
         <div id="img-container">
             <p>Website Visitors</p>
             <p>821</p>
-            <img src="https://pbs.twimg.com/media/EbbAWSuXQAIP3Vz?format=jpg&name=large" alt="elon musk meme"/>
+            <img src="" alt="elon musk meme"/>
         </div>
     )
 }
