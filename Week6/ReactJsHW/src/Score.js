d0747b372d31e6c48b9261818f37f8ecaa0a3029
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-08 19:09:17 -0400

    Add Students Homework

---
+++ Week6/ReactJsHW/src/Score.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Score(props) {
+    return (
+        props.object.scores.map((scoreInfo) =>
+            <li>Score Date: {scoreInfo.date}
+                <p class="score">Score: <strong>{scoreInfo.score}</strong></p>
+            </li>)
+    )
+}
