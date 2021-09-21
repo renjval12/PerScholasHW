18ed9a05bf659e0f39e4052cf52c19caafe8b952
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-21 15:22:12 -0400

    move files

--- ReactHW/src/App.js
+++ ReactHW/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Div1 from "./DivOne";
 
 export default function App() {
+const MyContext = React.createContext()
     return (
         <div>
+
             <Div1/>
         </div>
     );
