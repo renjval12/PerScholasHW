a6d972ed1827c6134ff78635fb8ff023e9a8fd57
renjval12 <reneejoyvalentine3@gmail.com>
2021-10-03 13:33:42 -0400

    Update Calculator.java

--- Mod3/src/calculatorPackage/Calculator.java
+++ Mod3/src/calculatorPackage/Calculator.java
@@ -48,61 +48,6 @@ public class Calculator {
 		scan.close(); // closes input stream
 	}
 
-//	function result() {
-//	    if (num1 === 6) {
-//	        return 'see ya later, gator'
-//	    }
-//	    else if (num1 === 1 || num1 === 2 || num1 === 3 || num1 === 4) {
-//	        var num2 = Number(prompt('Type in a number: '))
-//	        var num3 = Number(prompt('Type in a number: '))
-//	    }
-//	    else if (num1 === 5) {
-//	        var num2 = Number(prompt('Type in a number: '))
-//	        var num3 = Number(prompt('Type in a number: '))
-//	        var num4 = Number(prompt('Type in a number: '))
-//	        var num5 = Number(prompt('Type in a number: '))
-//	        var num6 = Number(prompt('Type in a number: '))
-//	    }
-//	    else {
-//	        return 'invalid entry'
-//	    }
-//	    switch (num1) {
-//	        case 1:
-//	            function add() {
-//	                return num2 + num3
-//	            }
-//	            return `${num2} + ${num3} = ${add()}`
-//	            break;
-//	        case 2:
-//	            function subtract() {
-//	                return num2 - num3
-//	            }
-//	            return `${num2} - ${num3} = ${subtract()}`
-//	            break;
-//	        case 3:
-//	            function multiply() {
-//	                return num2 * num3
-//	            }
-//	            return `${num2} * ${num3} = ${multiply()}`
-//	            break;
-//	        case 4:
-//	            function divide() {
-//	                return num2 / num3
-//	            }
-//	            return `${num2} / ${num3} = ${divide()}`
-//	            break;
-//	        case 5:
-//	            function average() {
-//	                let sum = num2 + num3 + num4 + num5 + num6
-//	                return  sum / 5
-//	            }
-//	            return `the average of ${num2}, ${num3}, ${num4}, ${num5}, and ${num6} = ${average()}`
-//	            break;
-//	        default:
-//	            return 'invalid entry'
-//	            break
-//	    }	
-
 	static int add(int y, int z) {
 		return y + z;
 	}
