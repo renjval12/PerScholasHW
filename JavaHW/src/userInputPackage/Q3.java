524777463bd6fdfe1ec041ffa532aad853a353a9
renjval12 <reneejoyvalentine3@gmail.com>
2021-10-03 09:28:13 -0400

    Update Q3.java

--- JavaHW/src/userInputPackage/Q3.java
+++ JavaHW/src/userInputPackage/Q3.java
@@ -8,15 +8,15 @@ public class Q3 {
 		Scanner scan = new Scanner(System.in); // opens input stream for user input
 		System.out.println("Type in a word: "); // prompt to the user to enter a number
 		String palText = scan.next(); // variable holds the user input number
-		palindrome(palText); // run armstrong(n) function
+		palindrome(palText.toUpperCase()); // run armstrong(n) function
 		scan.close(); // closes input stream
 	}
 
 	public static void palindrome(String n) {
-		StringBuffer reverseStr = new StringBuffer(n.toLowerCase());
+		StringBuffer reverseStr = new StringBuffer(n);
 		reverseStr.reverse();
 		String reverseStringify = reverseStr.toString();
-		if (n.toLowerCase().equals(reverseStringify)) {
+		if (n.equals(reverseStringify)) {
 			System.out.println(n + " is a Palindrome");
 		} else {
 			System.out.println(n + " is NOT a Palindrome");
